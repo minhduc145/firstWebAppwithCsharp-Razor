d32049d2eeac52c650d65ac8d3d5bacb12baad73
MyHP <ducle140503@gmail.com>
2024-10-11 11:26:13 +0700

    yeyyy

---
+++ HelloN1/Hello/Controllers/ProductsController.cs
@@ -0,0 +1,54 @@
+﻿using Hello.Services;
+using Hello.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hello.Controllers
+{
+    public class ProductsController : Controller
+    {
+        ProductServices service;
+        public ProductsController()
+        {
+            service = new ProductServices();
+        }
+        public IActionResult Index(String? keyWord)
+        {
+            ProductViewModel model = new ProductViewModel();
+            model.Products = service.GetProducts(keyWord);
+            if (!String.IsNullOrEmpty(keyWord))
+
+                model.keyWord = keyWord;
+
+            return View(model);
+        }
+        public IActionResult Delete(int id)
+        {
+            service.deleteP(id);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var p = service.getPbyId(id);
+            ProductViewModel model = new ProductViewModel();
+            model.ProductResponse = p;
+            return View(model);
+        }
+        [HttpPost]
+        public IActionResult Edit(ProductViewModel model)
+        {
+            var p = model.ProductResponse;
+            var rs = service.update(p);
+            return View(model);
+        }
+        [HttpPost]
+
+        public IActionResult Add(ProductViewModel model)
+        {
+            var p = model.ProductResponse;
+            var rs = service.add(p);
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ HelloN1/Hello/ViewModels/LopHocViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Hello.Models;
+
+namespace Hello.ViewModels
+{
+    public class LopHocViewModel
+    {
+        public List<LopHoc> listLopHoc { get; set; }
+
+        public List<SinhVien> listSV { get; set; }
+        public LopHoc current { get; set; }
+        public LopHoc LopHocResponse { get; set; }
+
+    }
+}
